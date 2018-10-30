4d45b310276600e2dd1891a2f0043674512f23e9
Hedaetul Islam <bulbulhedaytulislam@gmail.com>
2018-10-31 03:15:11 +0600

    Add files via upload

---
+++ lowercase to upper case.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 100h
+.data
+.code
+ 
+msg1 db 'input a uppercase alphabet: $'
+msg2 db 'upper case is:  $'
+main proc
+    
+    mov ax,@data ;initial data statement
+    mov ds,ax
+    
+    lea dx,msg1   ; load msg1 address
+    mov ah,9
+    int 21h
+    
+    mov ah,1
+    int 21h
+    
+    mov ah,2
+    mov bh,al
+    sub bh,20h
+    int 21h
+    
+    mov dl,0dh
+    int 21h
+    mov dl,0ah
+    int 21h
+    
+    lea dx,msg2   ; load msg1 address
+    mov ah,9      ; for string print used 9
+    int 21h
+    
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+main endp
+end main
